9d05ca66dede31e90b6697047ab291ca2e75d922
Elton <57287843+eltontay@users.noreply.github.com>
2021-05-30 21:17:43 +0800

    change error message to pop out only when after clicking submit button and the fields are empty

--- frontend/src/components/SigninForm.tsx
+++ frontend/src/components/SigninForm.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
+/*
+  Elton Notes 30 May 2021
+  Adding cross checking validation to backend database to prevent duplicate username
+*/
+
 interface FormData {
   username: string;
   password: string;
@@ -12,27 +17,12 @@ function SignInForm() {
   const {
     register,
     handleSubmit,
-    setError,
     formState: { errors },
-  } = useForm<FormData>({ mode: 'onChange' });
+  } = useForm<FormData>({ mode: 'onSubmit' });
 
-  const onSubmit = (data: FormData) => {
-    console.log(data);
-  };
-
-  React.useEffect(() => {
-    setError('username', {
-      type: 'manual',
-      message: 'Please enter your username',
-    });
-  }, [setError]);
-
-  React.useEffect(() => {
-    setError('password', {
-      type: 'manual',
-      message: 'Please enter your password',
-    });
-  }, [setError]);
+  const onSubmit = handleSubmit(({ username, password, remember }) => {
+    console.log(username, password, remember);
+  });
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center">
@@ -40,7 +30,7 @@ function SignInForm() {
         Log In{' '}
       </div>
       <div className="max-w-md w-full mx-auto mt-4 bg-white p-8 border border-gray-300">
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+        <form onSubmit={onSubmit} className="space-y-6">
           <div>
             <label
               htmlFor=""
@@ -54,7 +44,7 @@ function SignInForm() {
               style={{ borderColor: errors.username ? 'red' : '' }}
               className="w-full p-2 border border-gray-300 rounded mt-1"
             />
-            {errors.username && <p>{errors.username.message}</p>}
+            {errors.username && <p>"Please enter your username."</p>}
           </div>
           <div>
             <label
@@ -69,7 +59,7 @@ function SignInForm() {
               style={{ borderColor: errors.password ? 'red' : '' }}
               className="w-full p-2 border border-gray-300 rounded mt-1"
             />
-            {errors.password && <p>{errors.password.message}</p>}
+            {errors.password && <p>"Please enter your password."</p>}
           </div>
           <div className="flex items-center justify-between">
             <div className="flex items-center">
