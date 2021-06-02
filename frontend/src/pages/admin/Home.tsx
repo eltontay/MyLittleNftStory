3eee712d3706bae9b7bd92452dd3f5fcd998fd8b
Elton <57287843+eltontay@users.noreply.github.com>
2021-06-02 17:02:13 +0800

    did up a bit of animations and foundations for auctions at the home page

--- frontend/src/pages/admin/Home.tsx
+++ frontend/src/pages/admin/Home.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import NavBar from '../../components/Navbar';
-import Hero from '../../templates/Hero';
+import HeroAuction from '../../templates/HeroAuction';
+import LiveAuction from '../../templates/LiveAuction';
+import BouncingArrow from '../../templates/BouncingArrow';
 
 export default function HomePage() {
   return (
     <div>
       <NavBar />
-      <Hero />
+      <HeroAuction />
+      <BouncingArrow />
+
+      <div className="grid grid-cols-1 divide-y divide-black px-6">
+        <div>
+          <div className="animate-pulse ">
+            <div className="rounded-full bg-blue-700 h-2 w-2"></div>
+          </div>
+          <div className="flex pl-2">Live Auction</div>
+        </div>
+        <div />
+      </div>
+      <LiveAuction />
     </div>
   );
 }
---
+++ frontend/src/templates/HeroAuction.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+function HeroAuction() {
+  return (
+    <div>
+      <div className="h-screen">
+        <div className="justify-center h-4/5 p-4 mx-4 border border-black flex items-stretch flex-col items-center grid lg:grid-rows-6 lg:grid-cols-4  grid-flow-col gap-4">
+          <div className="justify-center h-full w-full p-4 border border-black mx-auto px-4 row-span-full col-span-2 place-self-center">
+            image
+          </div>
+          <div className="justify-center h-full w-full p-4 border border-black mx-auto px-4 row-start-1 col-span-2 place-self-center ">
+            title
+          </div>
+          <div className="justify-center h-full w-full p-4 border border-black mx-auto px-4 col-span-2 place-self-center">
+            Artist
+          </div>
+          <div className="justify-center h-full w-full p-4 border border-black mx-auto px-4 place-self-center">
+            Time Left
+          </div>
+          <div className="justify-center h-full w-full p-4 border border-black mx-auto px-4 place-self-center">
+            Current Bid
+          </div>
+          <div className="justify-center h-full w-full p-4 border border-black mx-auto px-4 place-self-center">
+            10,000 ADA
+          </div>
+          <div className="justify-center h-full w-full p-4 border border-black mx-auto px-4 place-self-center">
+            $18,000
+          </div>
+          <div className="justify-center h-full w-full p-4 border border-black mx-auto px-4 place-self-center">
+            Current Hodler
+          </div>
+          <div className="justify-center h-full w-full p-4 border border-black mx-auto px-4 place-self-center">
+            LittleHodler97
+          </div>
+          <div className="justify-center h-full w-full p-4 border border-black mx-auto px-4 place-self-center">
+            Bid
+          </div>
+          <div className="justify-center h-full w-full p-4 border border-black mx-auto px-4 place-self-center">
+            Read Story
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroAuction;
