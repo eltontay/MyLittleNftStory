c06fcee6ba60ab1a9f01c5bf1eba201b5e5c0b22
Lundy Pang <LundyPGK@gmail.com>
2021-06-06 16:34:28 +0800

    Created Registraion Component: Missing Form validation

--- frontend/src/pages/admin/Register.tsx
+++ frontend/src/pages/admin/Register.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
+import RegisterForm from '../../components/RegisterForm';
 
 export default function RegisterPage() {
   return (
     <div>
       <Navbar />
-      <h1>Register Page</h1>
+      <RegisterForm />
     </div>
   );
 }
