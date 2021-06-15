c5cf5bded5c607302e1d8474504cf19bb873bf9b
Elton <57287843+eltontay@users.noreply.github.com>
2021-06-15 15:53:07 +0800

    responsive edits for circle attributes

--- frontend/src/templates/CircleAttributes.tsx
+++ frontend/src/templates/CircleAttributes.tsx
@@ -3,42 +3,50 @@ import React from 'react';
 function CircleAttributes() {
   return (
     <div>
-      <div className="grid grid-cols-1 divide-y divide-black px-6 space-y-20">
-        <div>
-          <div className=" grid grid-cols-5 gap-4 p-6 flex flex-col justify-center items-stretch flex items-center">
-            <div className="relative rounded-full w-56 h-56 border border-black grid grid-cols-1 justify-center items-stretch flex items-center content-center">
-              <div className="grid grid-rows-3 gap-2 grid-flow-col text-center flex flex-wrap">
-                <div />
-                <div className="text-5xl font-bold">12</div>
-                <div className="text-xl font-medium">Live Auctions</div>
+      <div className="hidden md:block grid grid-cols-1 divide-y divide-black px-6 space-y-20 ">
+        <div className="flex relative grid grid-cols-5 py-8 justify-center items-stretch items-center ">
+          <div className="mx-4 h-36 lg:h-48 w-36 lg:w-48 relative rounded-full border border-black grid grid-cols-1 justify-center items-stretch flex items-center content-center ">
+            <div className="grid grid-rows-3 grid-flow-col text-center flex flex-wrap">
+              <div />
+              <div className="text-4xl lg:text-5xl font-bold">12</div>
+              <div className="text-base lg:text-lg font-medium">
+                Live Auctions
               </div>
             </div>
-            <div className="relative rounded-full w-56 h-56 border border-black grid grid-cols-1 justify-center items-stretch flex items-center content-center">
-              <div className="grid grid-rows-3 gap-2 grid-flow-col text-center flex flex-wrap">
-                <div />
-                <div className="text-5xl font-bold">24</div>
-                <div className="text-xl font-medium">Future Auctions</div>
+          </div>
+          <div className="mx-4 h-36 lg:h-48 w-36 lg:w-48 relative rounded-full border border-black grid grid-cols-1 justify-center items-stretch flex items-center content-center ">
+            <div className="grid grid-rows-3 grid-flow-col text-center flex flex-wrap">
+              <div />
+              <div className="text-4xl lg:text-5xl font-bold">24</div>
+              <div className="text-base lg:text-lg font-medium">
+                Future Auctions
               </div>
             </div>
-            <div className="relative rounded-full w-56 h-56 border border-black grid grid-cols-1 justify-center items-stretch flex items-center content-center">
-              <div className="grid grid-rows-3 gap-2 grid-flow-col text-center flex flex-wrap">
-                <div />
-                <div className="text-5xl font-bold">360</div>
-                <div className="text-xl font-medium">Bids placed in 24Hr</div>
+          </div>
+          <div className="mx-4 h-36 lg:h-48 w-36 lg:w-48 relative rounded-full border border-black grid grid-cols-1 justify-center items-stretch flex items-center content-center ">
+            <div className="grid grid-rows-3 grid-flow-col text-center flex flex-wrap">
+              <div />
+              <div className="text-4xl lg:text-5xl font-bold">360</div>
+              <div className="text-base lg:text-lg font-medium ">
+                Bids placed
               </div>
             </div>
-            <div className="relative rounded-full w-56 h-56 border border-black grid grid-cols-1 justify-center items-stretch flex items-center content-center">
-              <div className="grid grid-rows-3 gap-2 grid-flow-col text-center flex flex-wrap">
-                <div />
-                <div className="text-5xl font-bold">48</div>
-                <div className="text-xl font-medium">Cardano Artists</div>
+          </div>
+          <div className="mx-4 h-36 lg:h-48 w-36 lg:w-48 relative rounded-full border border-black grid grid-cols-1 justify-center items-stretch flex items-center content-center ">
+            <div className="grid grid-rows-3 grid-flow-col text-center flex flex-wrap">
+              <div />
+              <div className="text-4xl lg:text-5xl font-bold">48</div>
+              <div className="text-base lg:text-lg font-medium">
+                Cardano Artists
               </div>
             </div>
-            <div className="relative rounded-full w-56 h-56 border border-black grid grid-cols-1 justify-center items-stretch flex items-center content-center">
-              <div className="grid grid-rows-3 gap-2 grid-flow-col text-center flex flex-wrap">
-                <div />
-                <div className="text-5xl font-bold">1,260</div>
-                <div className="text-xl font-medium">Stories Heard</div>
+          </div>
+          <div className="mx-4 h-36 lg:h-48 w-36 lg:w-48 relative rounded-full border border-black grid grid-cols-1 justify-center items-stretch flex items-center content-center ">
+            <div className="grid grid-rows-3 grid-flow-col text-center flex flex-wrap">
+              <div />
+              <div className="text-4xl lg:text-5xl font-bold">1,260</div>
+              <div className="text-base lg:text-lg font-medium">
+                Stories Heard
               </div>
             </div>
           </div>
